dd7236581da2b39d6017a91a8bce105c6ae89d00
unknown <aal08281@CLK310-15.vtc.vsc.edu>
2015-01-13 19:57:58 -0500

    this is not this text

---
+++ Assignments/Lab_1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>hello</title>
+		<link rel="stylesheet" type="text/css" href="./css/styles.css" />
+	</head>
+	<body>
+		<div id="head">
+			<h1>Hello World</h1>
+		</div>
+		<div id="content">
+			<p> Hell world is the first project for many programming classes
+				because displaying a simple prompt is the best place to start.
+				Why everyone uses hello world I don't know.
+		</div>
+		<div id="footer">
+			<p> A random footer on a random page</p>
+		</div>
+	</body>
+</html>
+
+<?php
+	echo "hello World!";
+?>
