1b9560b65589b899435e8f1310424f09b4b65a19
unknown <aal08281@CLK310-15.vtc.vsc.edu>
2015-01-20 20:12:18 -0500

    lab 2

--- Assignments/Lab_2/lab_2.php
+++ Assignments/Lab_2/lab_2.php
@@ -6,7 +6,7 @@
  * This lab focuses on your understanding of arithmatic.
  *
  * @version 1.0
- * @author YOUR_NAME <YOUR_EMAIL_ADDRESS@vtc.edi>
+ * @author ARECK_LOGSDON <mynameisareck@gmail.com>
  * @since 20150120
  */
 
@@ -19,33 +19,56 @@
  * @param $float_value
  */
 
-define('GRAVITY', 9.8);
+define("GRAVITY", 9.8);
 
 function truncateFloat($float_value)
 {
-
+	$float_value = (int)($float_value*100)/100;
+	echo $float_value ."<br>";
 }
 
 /**
+ * Convert the temperature from farenheit to kelvin
+ *
+ * Takes the temperature in farenheit performs math operations
+ * and returns in Kelvin
+ *
  * @param $degrees_f
  */
 function farenheit2Kelvin($degrees_f)
 {
-
+	$degrees_k = ($degrees_f - 32)*(5/9)+273.15;
+	echo $degrees_k ."<br>";
 }
 
 /**
+ * Find the volume of a Dodecahedron
+ *
+ * Giving the area of a line segment return the volume of the Dodecahedron
+ *
  * @param $area
  */
 function dodecahedronVolume($area)
 {
-
+	$Volume = ((15+7*sqrt(5))/4)*pow($area,3);
+	echo $Volume ."<br>";
 }
 
 /**
+ * Return the impact velocity of a falling object
+ *
+ * Giving a height find the velocity at which the object with hit the ground on earth.
+ *
  * @param $height
  */
 function impactVelocity($height)
 {
-
+	$Velocity= sqrt(2*GRAVITY*$height);
+	echo $Velocity ."<br>";
 }
+
+
+ impactVelocity(98);
+ dodecahedronVolume(89);
+ farenheit2Kelvin(32);
+ truncateFloat(56.78978);
